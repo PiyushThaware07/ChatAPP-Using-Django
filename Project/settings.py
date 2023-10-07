bdde8e453122f02503be1b6f77a2afbebea61423
Piyush Thaware <piyushbthaware@gmail.com>
2023-10-08 03:03:04 +0530

    Vercel Setup remove

--- Project/settings.py
+++ Project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d4w@p9ooah(b=!5kj5*ca0_7*gibb%$h8_+*^ufim+$=1q8@m='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*','.vercel.app','now.sh']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -140,9 +139,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-# Vercel deploy
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
