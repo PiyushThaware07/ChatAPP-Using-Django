ba4a5338a3d63df8a9139bc9eeada430c7e028dd
Piyush Thaware <piyushbthaware@gmail.com>
2023-10-08 02:45:22 +0530

    Vercel Setup 3

--- Project/settings.py
+++ Project/settings.py
@@ -140,7 +140,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# Vercel COde
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
