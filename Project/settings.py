590e07e033cf35ba0838485a26b60cb6fd1b8b29
Piyush Thaware <piyushbthaware@gmail.com>
2023-10-08 16:20:18 +0530

    Templates Fix

--- Project/settings.py
+++ Project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
